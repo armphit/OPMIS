5b71ca957f4ab0b7bcb6202fa91c946f6f47b338
armphit <51408771+armphit@users.noreply.github.com>
2022-08-16 14:24:29 +0700

    update 16/08/2022

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { HttpService } from 'src/app/services/http.service';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-nav',
@@ -19,11 +20,14 @@ export class NavComponent {
       map((result) => result.matches),
       shareReplay()
     );
-
+    @ViewChild('drawer') public drawer!: MatDrawer;
   constructor(
     private breakpointObserver: BreakpointObserver,
     private http: HttpService
-  ) {}
+  ) {
+
+
+  }
 
   signOut(): void {
     this.http.alertLog('error', 'Logout Success');
@@ -35,17 +39,32 @@ export class NavComponent {
   panelOpd: boolean = false;
 
   togglePanel() {
+    if(window.innerWidth <600){
+      this.drawer.toggle()
+    }
     this.panelIpd = false;
     this.panelOpd = false;
   }
 
   togglePanel2() {
+    // if(window.innerWidth <600){
+    //   this.drawer.toggle()
+    // }
     this.panelIpd = true;
     this.panelOpd = false;
   }
 
   togglePanel3() {
+    // if(window.innerWidth <600){
+    //   this.drawer.toggle()
+    // }
     this.panelIpd = false;
     this.panelOpd = true;
+  }
+
+  checkinnerWidth() {
+    if(window.innerWidth <600){
+      this.drawer.toggle()
+    }
   }
 }
